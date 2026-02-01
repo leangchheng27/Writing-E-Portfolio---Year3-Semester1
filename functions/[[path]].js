db7bc464ca979dd1de2b4135182b6682231d0ddf
Phorn Leangchheng <leangchheng223h@gmail.com>
2026-02-01 23:11:55 +0700

    Fix Cloudflare Pages handler - use Cloudflare format not Node.js

---
+++ functions/[[path]].js
@@ -0,0 +1,5 @@
+import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
+
+import * as build from '../build/server/index.js';
+
+export const onRequest = createPagesFunctionHandler({ build });
