055c88bc8fa73d4301e562c3e4955320783205e0
Phorn Leangchheng <leangchheng223h@gmail.com>
2026-02-01 18:02:57 +0700

    Fix serverless handler to properly handle req/res in Vercel

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,14 @@
 import { createRequestHandler } from '@remix-run/node';
 import * as build from '../build/server/index.js';
 
-export default createRequestHandler({
+const requestHandler = createRequestHandler({
   build,
   mode: build.mode,
 });
+
+export default (req, res) => {
+  return requestHandler(req, res).catch(err => {
+    console.error('Request handler error:', err);
+    res.status(500).json({ error: 'Internal Server Error', details: err.message });
+  });
+};
